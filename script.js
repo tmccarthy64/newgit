3b9d4da4a86dc9a665c19a28b21192df7ed77335
Tim McCarthy <tmac3690@gmail.com>
2015-06-30 23:28:13 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,81 @@
 $(document).ready(function() {
-	$('#reddit').mouseover(function() {
-		$(this).fadeTo('0.5');
+	$('#reddit').mouseenter(function() { //increase opacity when mouse enters image
+		$('#reddit').fadeTo('slow', '1.0');
+	});
+	$('#reddit').mouseleave(function() { //decrease to original opacity when mouse leaves image
+		$('#reddit').fadeTo('slow', '0.5');
 	});
 });
+
+$(document).ready(function() {
+	$('#facebook').mouseenter(function() {
+		$('#facebook').fadeTo('slow', '1.0');
+	});
+	$('#facebook').mouseleave(function() {
+		$('#facebook').fadeTo('slow', '0.5');
+	});
+});
+
+$(document).ready(function() {
+	$('#ccri').mouseenter(function() {
+		$('#ccri').fadeTo('slow', '1.0');
+	});
+	$('#ccri').mouseleave(function() {
+		$('#ccri').fadeTo('slow', '0.5');
+	});
+});
+
+$(document).ready(function() {
+	$('#acclaris').mouseenter(function() {
+		$('#acclaris').fadeTo('slow', '1.0');
+	});
+	$('#acclaris').mouseleave(function() {
+		$('#acclaris').fadeTo('slow', '0.5');
+	});
+});
+
+$(document).ready(function() {
+	$('#boa').mouseenter(function() {
+		$('#boa').fadeTo('slow', '1.0');
+	});
+	$('#boa').mouseleave(function() {
+		$('#boa').fadeTo('slow', '0.5');
+	});
+});
+
+$(document).ready(function() {
+	$('#academy').mouseenter(function() {
+		$('#academy').fadeTo('slow', '1.0');
+	});
+	$('#academy').mouseleave(function() {
+		$('#academy').fadeTo('slow', '0.5');
+	});
+});
+
+$(document).ready(function() {
+	$('#camp').mouseenter(function() {
+		$('#camp').fadeTo('slow', '1.0');
+	});
+	$('#camp').mouseleave(function() {
+		$('#camp').fadeTo('slow', '0.5');
+	});
+});
+
+$(document).ready(function() {
+	$('#git').mouseenter(function() {
+		$('#git').fadeTo('slow', '1.0');
+	});
+	$('#git').mouseleave(function() {
+		$('#git').fadeTo('slow', '0.5');
+	});
+});
+
+$(document).ready(function() {
+	$('#stack').mouseenter(function() {
+		$('#stack').fadeTo('slow', '1.0');
+	});
+	$('#stack').mouseleave(function() {
+		$('#stack').fadeTo('slow', '0.5');
+	});
+});
+
